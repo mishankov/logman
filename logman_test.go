719d527b9c4d4bac16fa03fc20cd80e702b41ee1
Denis Mishankov <mishankov@mail.com>
2025-01-02 20:00:06 +0300

    separate format test

--- logman_test.go
+++ logman_test.go
@@ -8,6 +8,17 @@ import (
 func TestLogger_Debug(t *testing.T) {
 	buffer := &bytes.Buffer{}
 	timer := &FakeTimeProvider{}
+	formatter := NewDefaultFormatter(defaultFormat)
+
+	logger := NewLogger(buffer, timer, formatter)
+	logger.Debug("debug message")
+
+	AssertEqual(t, buffer.String(), "[2006-01-02 15:04:05 GMT-0700] [DEBUG] - debug message")
+}
+
+func TestDefaultFormatter(t *testing.T) {
+	buffer := &bytes.Buffer{}
+	timer := &FakeTimeProvider{}
 	formatter := NewDefaultFormatter("<_logLevel_> <_dateTime_>: _message_")
 
 	logger := NewLogger(buffer, timer, formatter)
