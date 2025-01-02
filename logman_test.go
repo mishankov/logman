851ce3312c50afa624c55095a3b924649dcf7f05
Denis Mishankov <mishankov@mail.com>
2025-01-02 21:15:14 +0300

    refactor tests

--- logman_test.go
+++ logman_test.go
@@ -68,18 +68,40 @@ func TestCompositeMessage(t *testing.T) {
 	formatter := logman.NewDefaultFormatter(logman.DefaultFormat)
 
 	logger := logman.NewLogger(buffer, timer, formatter)
-	logger.Debug("composite", "message")
-	logger.Info("composite", "message")
-	logger.Warn("composite", "message")
-	logger.Error("composite", "message")
-	logger.Fatal("composite", "message")
 
-	AssertEqual(t, buffer.String(), `[2006-01-02 15:04:05 GMT-0700] [Debug] - composite message
-[2006-01-02 15:04:05 GMT-0700] [Info] - composite message
-[2006-01-02 15:04:05 GMT-0700] [Warn] - composite message
-[2006-01-02 15:04:05 GMT-0700] [Error] - composite message
-[2006-01-02 15:04:05 GMT-0700] [Fatal] - composite message
-`)
+	tt := []struct {
+		logFunction func(...string)
+		want        string
+	}{
+		{
+			logFunction: logger.Debug,
+			want:        "[2006-01-02 15:04:05 GMT-0700] [Debug] - composite message\n",
+		},
+		{
+			logFunction: logger.Info,
+			want:        "[2006-01-02 15:04:05 GMT-0700] [Info] - composite message\n",
+		},
+		{
+			logFunction: logger.Warn,
+			want:        "[2006-01-02 15:04:05 GMT-0700] [Warn] - composite message\n",
+		},
+		{
+			logFunction: logger.Error,
+			want:        "[2006-01-02 15:04:05 GMT-0700] [Error] - composite message\n",
+		},
+		{
+			logFunction: logger.Fatal,
+			want:        "[2006-01-02 15:04:05 GMT-0700] [Fatal] - composite message\n",
+		},
+	}
+
+	message := []string{"composite", "message"}
+
+	for _, test := range tt {
+		test.logFunction(message...)
+		AssertEqual(t, buffer.String(), test.want)
+		buffer.Reset()
+	}
 }
 
 func TestFormatedMessages(t *testing.T) {
@@ -90,45 +112,36 @@ func TestFormatedMessages(t *testing.T) {
 	logger := logman.NewLogger(buffer, timer, formatter)
 
 	tt := []struct {
-		message     string
-		formats     []string
 		logFunction func(string, ...any)
 		want        string
 	}{
 		{
-			message:     "my %v message %v",
-			formats:     []string{"awesome", "here"},
 			logFunction: logger.Debugf,
 			want:        "[2006-01-02 15:04:05 GMT-0700] [Debug] - my awesome message here\n",
 		},
 		{
-			message:     "my %v message %v",
-			formats:     []string{"awesome", "here"},
 			logFunction: logger.Infof,
 			want:        "[2006-01-02 15:04:05 GMT-0700] [Info] - my awesome message here\n",
 		},
 		{
-			message:     "my %v message %v",
-			formats:     []string{"awesome", "here"},
 			logFunction: logger.Warnf,
 			want:        "[2006-01-02 15:04:05 GMT-0700] [Warn] - my awesome message here\n",
 		},
 		{
-			message:     "my %v message %v",
-			formats:     []string{"awesome", "here"},
 			logFunction: logger.Errorf,
 			want:        "[2006-01-02 15:04:05 GMT-0700] [Error] - my awesome message here\n",
 		},
 		{
-			message:     "my %v message %v",
-			formats:     []string{"awesome", "here"},
 			logFunction: logger.Fatalf,
 			want:        "[2006-01-02 15:04:05 GMT-0700] [Fatal] - my awesome message here\n",
 		},
 	}
 
+	message := "my %v message %v"
+	formats := []string{"awesome", "here"}
+
 	for _, test := range tt {
-		test.logFunction(test.message, test.formats[0], test.formats[1])
+		test.logFunction(message, formats[0], formats[1])
 		AssertEqual(t, buffer.String(), test.want)
 		buffer.Reset()
 	}
