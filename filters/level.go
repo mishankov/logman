ace33405b4a2561364f773f8b5efbd31453c3906
Denis Mishankov <mishankov@mail.com>
2025-01-10 14:23:14 +0300

    add docs for log level filter

--- filters/level.go
+++ filters/level.go
@@ -2,14 +2,17 @@ package filters
 
 import "github.com/mishankov/logman"
 
+// LevelFilter implements Filter interface
 type LevelFilter struct {
 	level logman.LogLevel
 }
 
+// NewLevelFilter creates a new LevelFilter
 func NewLevelFilter(level logman.LogLevel) LevelFilter {
 	return LevelFilter{level: level}
 }
 
+// Filter returns true for messges that on the same or higher log level than LevelFilter.level
 func (lf LevelFilter) Filter(level logman.LogLevel, _, _ string) bool {
 	return level >= lf.level
 }
