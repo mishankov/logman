4a792b67cfff723706d288a66e4e68579132dad5
Denis Mishankov <mishankov@mail.com>
2025-01-12 16:33:47 +0300

    revert api changes

--- formatters/json.go
+++ formatters/json.go
@@ -16,13 +16,13 @@ func NewJSONFormatter() JSONFormatter {
 }
 
 type jsonLog struct {
-	LogLevel     string `json:"logLevel"`
-	DateTime     string `json:"dateTime"`
-	CallLocation string `json:"callLocation"`
+	LogLevel     string `json:"log_level"`
+	DateTime     string `json:"date_time"`
+	CallLocation string `json:"call_location"`
 	Message      string `json:"message"`
 }
 
-// Format formats log message as JSON with keys: logLevel, dateTime, callLocation and message.
+// Format formats log message as JSON with keys: log_level, date_time, call_location and message.
 func (jf JSONFormatter) Format(logLevel logman.LogLevel, dateTime time.Time, callLocation string, message string) string {
 	res, _ := json.Marshal(jsonLog{logLevel.String(), dateTime.Format(DefaultTimeLayout), callLocation, message})
 
